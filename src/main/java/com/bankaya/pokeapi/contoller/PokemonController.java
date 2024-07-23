d0ff24f08d9f65eac9d3fd66fa1e17a6b56e0399
ORLANDO-VECTOR\orlando <killerblack_25@hotmail.com>
2024-07-23 14:49:24 -0600

    Removing unused imports in controller and update dependencies in pom

--- src/main/java/com/bankaya/pokeapi/contoller/PokemonController.java
+++ src/main/java/com/bankaya/pokeapi/contoller/PokemonController.java
@@ -1,19 +1,14 @@
 package com.bankaya.pokeapi.contoller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.bankaya.pokeapi.model.Ability;
-import com.bankaya.pokeapi.model.Item;
 import com.bankaya.pokeapi.model.Pokemon;
-import com.bankaya.pokeapi.model.PokemonLimit;
 import com.bankaya.pokeapi.service.PokemonService;
 
 @RestController
